a86c8d7c84ecb46a88441fcc8d09d350cec0efba
Guy VItrak <guyv22@gmail.com>
2025-05-23 17:37:59 +0300

    .

--- q2/tabular_Q.py
+++ q2/tabular_Q.py
@@ -1,3 +1,5 @@
+from importlib.metadata import distribution
+
 import gym
 import numpy as np
 
@@ -20,19 +22,36 @@ for i in range(num_episodes):
     rAll = 0 # Total reward during current episode
     d = False
     j = 0
+
+    current_observation = s
+    current_action = 0 # just for init
     #The Q-Table learning algorithm
     while j < 99:
         j+=1
         # TODO: Implement Q-Learning
         # 1. Choose an action by greedily (with noise) picking from Q table
+        best_action = np.argmax(Q[s])
+        epsilon = 0.01
+        next_action_distribution = np.zeros(env.action_space.n) + (1 - epsilon) / env.action_space.n
+        next_action_distribution[best_action] += epsilon
+        new_action = np.random.choice(4, p=next_action_distribution)
         # 2. Get new state and reward from environment
+        next_state, reward, terminated, truncated = env.step(new_action)
         # 3. Update Q-Table with new knowledge
+        gamma_t = reward + y * np.max(Q[next_state]) - Q[s][new_action]
+        Q[s][new_action] += + lr * gamma_t
         # 4. Update total reward
+        rAll += reward
         # 5. Update episode if we reached the Goal State
+        if terminated:
+            break
+
+        s = next_state
+
     
     rList.append(rAll)
 
 # Reports
 print("Score over time: " +  str(sum(rList)/num_episodes))
 print("Final Q-Table Values")
-print Q
+print(Q)
